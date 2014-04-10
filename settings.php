2bc88ad5b763597bcf978a8711d2bfc26beee935
ansj <andreas.sjoberg@telepo.com>
2014-04-10 14:08:37 +0200

    Supplied the table layout

--- settings.php
+++ settings.php
@@ -11,3 +11,16 @@ interface crypto {
 	const shaker = "INSERT_A_RANDOM_STRING_OF_AT_LEAST_32_CHARACTERS_HERE";
 	const debug  = "INSERT_ANOTHER_RANDOM_STRING_HERE";
 }
+
+/* * *
+Add a table to your database
+
+CREATE TABLE IF NOT EXISTS `YOUR_TABLE_NAME` (
+  `key` varchar(64) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+
+* * */
